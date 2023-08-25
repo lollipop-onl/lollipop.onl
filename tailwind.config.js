73d6002dcac0d6a9052fcdc206dc635b30f83b85
simochee <lollipop@simochee.net>
2023-08-25 20:08:24 +0900

    ⚡ remove astro

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,26 @@
-import { family as ZenMaruGothic } from '@fontsource/zen-maru-gothic/metadata.json' assert { type: "json" };
+import typography from "@tailwindcss/typography";
+import { family as ZenMaruGothic } from "@fontsource/zen-maru-gothic/metadata.json" assert { type: "json" };
+import { addDynamicIconSelectors } from "@iconify/tailwind";
 
 /** @type {import('tailwindcss').Config} */
 const config = {
-  content: ["./src/**/*.{astro,ts}"],
+  content: ["./src/**/*.{svelte,ts}"],
   theme: {
     fontFamily: {
-      body: [ZenMaruGothic, 'sans-serif'],
+      body: [ZenMaruGothic, "sans-serif"],
     },
     extend: {
+      colors: {
+        background: "#0F172A",
+        foreground: "#1E293B",
+        link: {
+          DEFAULT: "#38bdf8",
+        },
+        body: "#eee",
+      },
     },
   },
-  plugins: [],
+  plugins: [typography, addDynamicIconSelectors()],
 };
 
 export default config;
