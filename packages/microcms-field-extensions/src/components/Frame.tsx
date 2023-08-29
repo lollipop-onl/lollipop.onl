d7df648d1783a7dcc739c06ea24b562bdad74dc7
simochee <lollipop@simochee.net>
2023-08-30 02:18:46 +0900

    ✨ add base of editor

---
+++ packages/microcms-field-extensions/src/components/Frame.tsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+import { setupFieldExtension } from 'microcms-field-extension-api';
+import { useMeasure } from 'react-use';
+
+type Props = {
+  origin: string;
+  children: React.ReactNode;
+};
+
+export const Frame: React.FC<Props> = ({ origin, children }) => {
+  const [count, setCount] = useState(0);
+  const [ref, { height }] = useMeasure<HTMLDivElement>();
+
+  useEffect(() => {
+    return setupFieldExtension({
+      origin,
+      width: '100%',
+      height,
+    });
+  }, [height]);
+
+  return (
+    <div ref={ref}>
+      {children}
+      <div>
+        <button onClick={() => setCount((count) => count + 1)}>Add Item</button>
+      </div>
+      <ul>
+        {Array.from({ length: count }).map((_, i) => (
+          <li key={i}>{i}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
--- packages/microcms-field-extensions/src/index.tsx
+++ packages/microcms-field-extensions/src/index.tsx
@@ -1,11 +1,38 @@
+import { Frame } from '@components/Frame';
 import { createRoot } from 'react-dom/client';
+import { createHashRouter, Outlet, RouterProvider } from 'react-router-dom';
 
 export const render = (rootEl: HTMLElement | null) => {
   if (!rootEl) return;
 
-  console.log(rootEl);
+  const { origin } = rootEl.dataset;
+
+  if (!origin) {
+    throw new TypeError('data-origin is required');
+  }
+
+  const router = createHashRouter([
+    {
+      path: '/',
+      element: (
+        <Frame origin={origin}>
+          <Outlet />
+        </Frame>
+      ),
+      children: [
+        {
+          path: 'hello',
+          element: <p>world</p>,
+        },
+        {
+          path: '*',
+          element: <p>404</p>,
+        },
+      ],
+    },
+  ]);
 
   const root = createRoot(rootEl);
 
-  root.render(<p>hello</p>);
+  root.render(<RouterProvider router={router} />);
 };
