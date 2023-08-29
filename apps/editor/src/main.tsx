408ad944c1edde99d52e3f33239a5ec8ec334a6c
simochee <lollipop@simochee.net>
2023-08-29 22:19:13 +0900

    ✨ add editor field

---
+++ apps/editor/src/main.tsx
@@ -0,0 +1,6 @@
+import { createRoot } from 'react-dom/client';
+import 'tailwindcss/tailwind.css';
+
+const root = createRoot(document.getElementById('root')!);
+
+root.render(<p>Hello, World</p>);
---
+++ apps/editor/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+const config = {
+  content: ['./index.html', './src/**/*.{ts,tsx}'],
+  theme: {
+    fontFamily: {
+      sans: ['IBM Plex Sans JP', 'sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
+
+export default config;
