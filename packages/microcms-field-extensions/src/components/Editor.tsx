fa5c964a780365105cf99d6fc92203c8ca11e1ee
simochee <lollipop@simochee.net>
2023-08-30 11:04:20 +0900

    ✨ add sync value

--- packages/microcms-field-extensions/src/components/Editor.tsx
+++ packages/microcms-field-extensions/src/components/Editor.tsx
@@ -1,19 +1,29 @@
 import { useFieldExtension } from '@hooks/useFieldExtension';
+import type { editor } from 'monaco-editor';
 import { Editor as MonacoEditor } from '@monaco-editor/react';
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 
 type Props = {};
 
 export const Editor: React.FC<Props> = () => {
-  const { message, postData } = useFieldExtension();
+  const editorRef = useRef<editor.IStandaloneCodeEditor>();
+  const { message, postData } = useFieldExtension<string | undefined>();
 
-  useEffect(() => console.log(message), [message]);
+  useEffect(() => {
+    editorRef.current?.setValue(message?.data ?? '');
+  }, [message]);
 
   return (
     <MonacoEditor
       height={500}
       defaultLanguage="markdown"
       defaultValue={(message?.data as string) ?? ''}
+      options={{
+        minimap: { enabled: false },
+        lineNumbers: 'off',
+        wordWrap: 'on',
+      }}
+      onMount={(editor) => (editorRef.current = editor)}
       onChange={(value) => postData({ data: value })}
     />
   );
