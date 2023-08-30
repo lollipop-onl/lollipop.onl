374cfc26b77d19655f4e8694eab13196edf9c569
simochee <lollipop@simochee.net>
2023-08-30 16:05:21 +0900

    ✨ setup partytown

--- apps/www/astro.config.ts
+++ apps/www/astro.config.ts
@@ -1,6 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import partytown from '@astrojs/partytown';
 
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    partytown({
+      config: {
+        forward: ['dataLayer.push'],
+      },
+    }),
+  ],
 });
