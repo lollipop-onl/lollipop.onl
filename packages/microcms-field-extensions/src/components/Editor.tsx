086ebcc4edca3f7bd5acb9cdb2cb3b0f98562e4c
simochee <lollipop@simochee.net>
2023-08-30 12:42:40 +0900

    ⚡ fix style

--- packages/microcms-field-extensions/src/components/Editor.tsx
+++ packages/microcms-field-extensions/src/components/Editor.tsx
@@ -9,35 +9,41 @@ export const Editor: React.FC<Props> = () => {
   const editorRef = useRef<editor.IStandaloneCodeEditor>();
   const [contentHeight, setContentHeight] = useState(0);
   const { message, postData } = useFieldExtension<string | undefined>();
+  const [data, setData] = useState('');
 
   useEffect(() => {
     editorRef.current?.setValue(message?.data ?? '');
   }, [message]);
 
   return (
-    <div className="overflow-hidden rounded border">
-      <div style={{ height: `${contentHeight}px` }}>
-        <MonacoEditor
-          defaultLanguage="markdown"
-          defaultValue={(message?.data as string) ?? ''}
-          options={{
-            minimap: { enabled: false },
-            lineNumbers: 'off',
-            wordWrap: 'on',
-            scrollbar: { vertical: 'hidden', horizontal: 'hidden' },
-            scrollBeyondLastLine: false,
-          }}
-          onMount={(editor) => {
-            editorRef.current = editor;
+    <div>
+      <div className="overflow-hidden rounded border">
+        <div style={{ height: `${contentHeight}px` }}>
+          <MonacoEditor
+            defaultLanguage="markdown"
+            defaultValue={(message?.data as string) ?? ''}
+            options={{
+              minimap: { enabled: false },
+              lineNumbers: 'off',
+              wordWrap: 'on',
+              scrollbar: { vertical: 'hidden', horizontal: 'hidden' },
+              scrollBeyondLastLine: false,
+            }}
+            onMount={(editor) => {
+              editorRef.current = editor;
 
-            editor.onDidContentSizeChange((e) => {
-              setContentHeight(e.contentHeight);
-            });
-          }}
-          onChange={(value) => postData({ data: value })}
-        />
+              editor.onDidContentSizeChange((e) => {
+                setContentHeight(e.contentHeight);
+              });
+            }}
+            onChange={(value = '') => {
+              postData({ data: value });
+              setData(value);
+            }}
+          />
+        </div>
       </div>
-      <p className="mt-1 text-right text-sm">{message?.data?.length || 0}</p>
+      <p className="mt-1 text-right text-sm text-gray-500">{data.length}</p>
     </div>
   );
 };
